ee204539fb7822bb31d14172698ba6f9dd9f65ff
Juan Sebastian Ospina Losada <jolosada1@gmail.com>
2023-02-15 14:37:03 -0500

    reasignacion y redeclaracion

---
+++ src/scope/re.js
@@ -0,0 +1,32 @@
+var firstName; //Undefined
+firstName = 'Oscar';
+console.log(firstName);
+
+var lastName = "David"; //Declarar y asignar
+lastName = "Ana"; //Reasignar
+console.log(lastName);
+
+var secondName = 'David'; //Declarar y asignar
+var secondName = 'Ana'; //Reasignar
+console.log(secondName);
+
+//Let
+let fruit = 'Apple'; //Declarar y asignar
+fruit = 'Kiwi'; //Reasignar
+console.log(fruit);
+
+let fruit = 'banana';
+console.log(fruit);
+
+//Const
+const animal = 'dog'; //Declarar y asignar
+animal = 'cat'; //Reasignar
+const animal = 'Snake';
+console.log(animal);
+
+const vehicles = [];
+vehicles.push("cart1");
+console.log(vehicles);
+
+vehicles.pop();
+console.log(vehicles);
